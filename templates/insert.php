f8dc75fc6fed204d1a279a10052c99b0b0991d65
Rohit Mehta <rohit9mehta@berkeley.edu>
2020-06-08 09:42:01 -0700

    fixing php

--- templates/insert.php
+++ templates/insert.php
@@ -1,53 +1,41 @@
-<html>
-
-<body>
-
- 
-
- 
-
 <?php
-
-$con = mysql_connect("162.244.65.29:8443","admin_","The$ky1isblu3#");
-
-if (!$con)
-
-  {
-
-  die('Could not connect: ' . mysql_error());
-
-  }
-
- 
-
-mysql_select_db("admin_", $con);
-
- 
-
-$sql="INSERT INTO userInput (name, number, email, preferences)
-
-VALUES
-
-('$_POST[name]','$_POST[number]', '$_POST[email]', '$_POST[preferences]')";
-
- 
-
-if (!mysql_query($sql,$con))
-
-  {
-
-  die('Error: ' . mysql_error());
-
-  }
-
-echo "1 record added";
-
- 
-
-mysql_close($con)
-
+$name = $_POST['name'];
+$number = $_POST['number'];
+$email = $_POST['email'];
+$preferences = $_POST['preferences'];
+if (!empty($name) || !empty($number) || !empty($email) || !empty($preferences)) {
+ $host = "localhost";
+    $dbUsername = "admin_";
+    $dbPassword = "The\$ky1isblu3#";
+    $dbname = "userInput";
+    //create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+    if (mysqli_connect_error()) {
+     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    } else {
+     $SELECT = "SELECT email From register Where email = ? Limit 1";
+     $INSERT = "INSERT Into register (name, number, email, preferences) values(?, ?, ?, ?)";
+     //Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+     if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("siss", $name, $number, $email, $preferences);
+      $stmt->execute();
+      echo "New record inserted sucessfully";
+     } else {
+      echo "Someone already registered using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
 ?>
-
-</body>
-
-</html>
